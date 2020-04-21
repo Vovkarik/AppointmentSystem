1abbb8882e57fb8129f184e6b862edc69fb6902f
beatrate <inik4us@gmail.com>
2020-04-21 17:35:57 +0300

    Added user profile.

---
+++ AppointmentSystem.Data/Queries/GetCurrentAppointmentsForUserQuery.cs
@@ -0,0 +1,46 @@
+﻿using AppointmentSystem.Core.Dto;
+using CqrsSpirit;
+using CqrsSpirit.Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Data.Queries
+{
+	public interface IGetCurrentAppointmentsForUserQuery : IQuery
+	{
+		Task<IEnumerable<AvailableTimeSlot>> ExecuteAsync(string userId);
+	}
+
+	public class GetCurrentAppointmentsForUserQuery : ObjectQueryBase<ApplicationDbContext>, IGetCurrentAppointmentsForUserQuery
+	{
+		public GetCurrentAppointmentsForUserQuery(ApplicationDbContext dbContext) : base(dbContext)
+		{
+
+		}
+
+		public async Task<IEnumerable<AvailableTimeSlot>> ExecuteAsync(string userId)
+		{
+			DateTime now = DateTime.UtcNow.Date;
+			return await DbContext.AppointmentSlots
+				.Include(slot => slot.Appointment)
+				.Where(slot => slot.Appointment != null && slot.Appointment.UserId == userId && slot.StartTime.Date >= now)
+				.OrderBy(slot => slot.StartTime)
+				.Select(slot => new AvailableTimeSlot
+				{
+					Id = slot.Id,
+					StartTime = slot.StartTime,
+					Doctor = new AvailableDoctor
+					{
+						Id = slot.Doctor.Id,
+						Name = slot.Doctor.Name,
+						Surname = slot.Doctor.Surname,
+						MiddleName = slot.Doctor.MiddleName,
+						Category = slot.Doctor.DoctorCategory
+					}
+				}).ToListAsync();
+		}
+	}
+}
---
+++ AppointmentSystem/Pages/User/Profile.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentSystem.Core;
+using AppointmentSystem.Core.Dto;
+using AppointmentSystem.Core.Entities;
+using AppointmentSystem.Core.Services;
+using AppointmentSystem.Data.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppointmentSystem.Pages.User
+{
+	[Authorize(AuthenticationSchemes = AuthenticationSchemes.User)]
+    public class ProfileModel : PageModel
+    {
+		private readonly IUserIdentityService identityService;
+		private readonly IGetCurrentAppointmentsForUserQuery getAppointmentsQuery;
+
+		public List<AvailableTimeSlot> Slots { get; } = new List<AvailableTimeSlot>();
+
+		public ProfileModel(IUserIdentityService identityService, IGetCurrentAppointmentsForUserQuery getAppointmentsQuery)
+		{
+			this.identityService = identityService;
+			this.getAppointmentsQuery = getAppointmentsQuery;
+		}
+
+		public async Task<IActionResult> OnGet()
+        {
+			ApplicationUser user = await identityService.GetCurrentUserAsync();
+			Slots.AddRange(await getAppointmentsQuery.ExecuteAsync(user.Id));
+			return Page();
+        }
+    }
+}
