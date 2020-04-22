603194456cf4858592d6e8a542930772c8a2274b
beatrate <inik4us@gmail.com>
2020-04-22 13:31:23 +0300

    Added appointments view to admin.

---
+++ AppointmentSystem.Data/Queries/GetAllPendingAppointmentsQuery.cs
@@ -0,0 +1,45 @@
+﻿using AppointmentSystem.Core.Dto;
+using CqrsSpirit;
+using CqrsSpirit.Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Data.Queries
+{
+	public interface IGetAllPendingAppointmentsQuery : IQuery
+	{
+		Task<IEnumerable<AvailableTimeSlot>> ExecuteAsync();
+	}
+
+	public class GetAllPendingAppointmentsQuery : ObjectQueryBase<ApplicationDbContext>, IGetAllPendingAppointmentsQuery
+	{
+		public GetAllPendingAppointmentsQuery(ApplicationDbContext dbContext) : base(dbContext)
+		{
+		}
+
+		public async Task<IEnumerable<AvailableTimeSlot>> ExecuteAsync()
+		{
+			DateTime now = DateTime.UtcNow;
+			return await DbContext.AppointmentSlots
+				.Include(slot => slot.Appointment)
+				.Where(slot => slot.Appointment != null && slot.StartTime > now)
+				.OrderBy(slot => slot.StartTime)
+				.Select(slot => new AvailableTimeSlot
+				{
+					Id = slot.Id,
+					StartTime = slot.StartTime,
+					Doctor = new AvailableDoctor
+					{
+						Id = slot.Doctor.Id,
+						Name = slot.Doctor.Name,
+						Surname = slot.Doctor.Surname,
+						MiddleName = slot.Doctor.MiddleName,
+						Category = slot.Doctor.DoctorCategory
+					}
+				}).ToListAsync();
+		}
+	}
+}
---
+++ AppointmentSystem/Pages/Admin/Appointments/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentSystem.Core.Dto;
+using AppointmentSystem.Data.Queries;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppointmentSystem.Pages.Admin.Appointments
+{
+    public class IndexModel : PageModel
+    {
+		private readonly IGetAllPendingAppointmentsQuery getAppointmentsQuery;
+
+		public List<AvailableTimeSlot> Slots { get; } = new List<AvailableTimeSlot>();
+
+		public IndexModel(IGetAllPendingAppointmentsQuery getAppointmentsQuery)
+		{
+			this.getAppointmentsQuery = getAppointmentsQuery;
+		}
+
+		public async Task<IActionResult> OnGet()
+        {
+			Slots.AddRange(await getAppointmentsQuery.ExecuteAsync());
+			return Page();
+        }
+    }
+}
